d20cf63c7d244376fcd432c7187f2649178323a3
Max Rubenacker <shuuki@gmail.com>
2016-05-27 17:02:31 -0600

    integrate annihilation into index js

--- script/index.js
+++ script/index.js
@@ -1,4 +1,41 @@
-//"use strict";
+/*
+INIT
+*/
+
+"use strict";
+
+(function($) {
+
+/*
+STICKY NAVIGATION
+*/
+
+$(window).scroll(function() {
+	var view = $("body"),
+		breakpoint = $(window).height() - $("nav").height();
+
+	if (view.scrollTop() > breakpoint) {
+		$("body").css("background", "magenta");
+		$("nav").addClass("fixed");
+	} else {
+		$("body").css("background", "rgb(232,232,232)");
+		$("nav").removeClass("fixed");
+	}
+});
+
+
+/*
+FULL HEIGHT SECTIONS
+*/
+
+// set sections to window height
+$("section").css("min-height", $(window).height());
+
+// update section height on resize
+$(window).resize(function() {
+	$("section").css("min-height", $(window).height());
+});
+
 
 /*
 PACKERY
@@ -9,28 +46,14 @@ var $grid = $('.gallery').packery({
 	itemSelector: '.piece',
   gutter: 0
 });
-// layout Packery after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.packery();
-});
 
-
-/*
-STICKY NAVIGATION
-*/
-
-$(window).scroll(function() {
-	var view = $("body");
-	var breakpoint = $(window).height() - 40; // height of #top
-
-	if (view.scrollTop() > breakpoint) {
-		$("#intro").css("background", "magenta");
-		$("nav").addClass("fixed");
-	} else {
-		$("#intro").css("background", "rgb(232,232,232)");
-		$("nav").removeClass("fixed");
+// lay out Packery after each image loads
+$grid.imagesLoaded().progress(
+	function() {
+	  $grid.packery();
 	}
-});
+);
+
 
 /*
 STYLESHEET SWITCHER
@@ -43,7 +66,7 @@ Inspired by Kelvin Luck's jQuery Stylesheet Switcher:
 http://2008.kelvinluck.com/assets/jquery/styleswitch/toggle.html
 */
 
-(function($) {
+
 
 	// load up the current stylesheets
 	var styles = $("link[class*='theme']"),
@@ -66,9 +89,71 @@ http://2008.kelvinluck.com/assets/jquery/styleswitch/toggle.html
 		}
 	};
 
-})(jQuery);
 
 // switching actor, listening in for a click on the "#contrast" element
 $("#contrast").bind("click", function(e) {
 	$.styleSwitch();
 });
+
+
+
+/*** SELF ANNIHILATION */
+
+//document.getElementsByTagName("p"),
+
+var words = $("#annihilation p"),
+	counter = $("#annihilation #counter"),
+	chars = [" ", "!", "?", ".", "-", "/", ":",";", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+function thing() {
+	for (var i = 0; i < words.length; i ++) {
+		var buffer = [],
+			entropy = words[i].innerHTML[Math.floor(Math.random() * words[i].innerHTML.length)]
+		for (var k = 0; k < words[i].innerHTML.length; k++) {
+			if (Math.floor(Math.random() * 10000) == 1) {
+				buffer.push(entropy);
+			} else if (Math.floor(Math.random() * 500000) == 1) {
+				buffer.push(shuffle(chars));
+			} else if (Math.floor(Math.random() * 500000) == 1) {
+				//delete
+			} else {
+				buffer.push(words[i].innerHTML[k]);
+			}
+		}
+		words[i].innerHTML = buffer.join("");
+	}
+}
+
+function shuffle(list) {
+	return list[Math.floor(Math.random() * list.length)];
+};
+
+/*** SIMULATION */
+var simulation,
+	simRunning = false,
+	simTurns = 0,
+	sim = function(actions, interval) {
+	if (simRunning === false) {
+		simulation = setInterval(function() {
+			simRunning = true;
+			simTurns++;
+			actions();
+		}, interval);
+	} else {
+		return;
+	}
+};
+sim.stop = function() {
+	clearInterval(simulation);
+	simRunning = false;
+};
+
+// fire off the SIM
+sim(function() {
+	thing();
+}, 250);
+
+
+
+
+})(jQuery);
