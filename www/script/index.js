b904d4ee79a18d60156b80e4e18c7ffffd132ab1
Max Rubenacker <shuuki@gmail.com>
2016-05-29 14:03:26 -0600

    recompile www

--- www/script/index.js
+++ www/script/index.js
@@ -4,6 +4,37 @@ INIT
 
 "use strict";
 
+
+
+
+
+/***
+KEYPAD
+*/
+
+
+
+function addCode(key) {
+	var code = document.forms[0].code;
+  if(code.value.length < 6) {
+		code.value = code.value + key;
+	}
+  if(code.value.length == 6) {
+    document.getElementById("message").style.opacity = "1";
+    //setTimeout(submitForm,1000);
+  }
+}
+
+function submitForm() {
+	document.forms[0].submit();
+}
+
+function emptyCode() {
+	document.forms[0].code.value = "";
+}
+
+
+
 (function($) {
 
 
@@ -45,6 +76,7 @@ FULL HEIGHT SECTIONS
 $(window).resize(function() {
 //	$("section").css("min-height", $(window).height());
 });
+
 
 
 /*
