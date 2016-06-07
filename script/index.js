153d7fea217d1e067e14a7d41079c04c8fd139ec
Max Rubenacker <shuuki@gmail.com>
2016-06-07 13:37:34 -0600

    simplify nav links, move into a simple script that loads and adds a hash to window location

--- script/index.js
+++ script/index.js
@@ -7,3 +7,9 @@ INIT
 (function($) {
 	// fire module when jquery loads
 })(jQuery);
+$("nav links a").click(function() {
+	var destination = $(this).attr("href");
+	$("#longer").load(destination);
+	window.location.hash = destination.substr(0, destination.length)
+	return false;
+});
