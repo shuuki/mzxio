fdf50c904999b028859cb97a8122cc4c2b0a6a1f
Max Rubenacker <shuuki@gmail.com>
2016-06-12 11:26:20 -0600

    delete old keypad code php file

--- code.php
+++
@@ -1,9 +0,0 @@
-<?php
-switch($_GET['code']){
-	case 'FFFFFF': header('Location: key/white.html'); break;
-	case '000000': header('Location: key/black.html'); break;
-	case 'FF0000': header('Location: key/red.html'); break;
-	case '0000FF': header('Location: key/blue.html'); break;
-	default: header('Location: index.html'); break;
-}
-?>
