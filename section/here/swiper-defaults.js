c4a53a600ced1d56ff5ec8b81bfd2ea74d3c1d36
Max Rubenacker <shuuki@gmail.com>
2016-07-01 19:18:42 -0600

    trying to abstract swiper on here section, currently broken

---
+++ section/here/swiper-defaults.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+  centeredSlides: true,
+  grabCursor: true,
+  keyboardControl: true,
+  loop: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  scrollbarHide: true,
+  slidesPerView: 'auto',
+  spaceBetween: 18
+  //,
+  //preloadImages: false,
+  //lazyLoading: true,
+  //autoplay: 30000,
+  //autoplayDisableOnInteraction: true
+});
