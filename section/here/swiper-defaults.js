bcdb63ca3c19db2e183dd5d1e0380f467286aa77
Max Rubenacker <shuuki@gmail.com>
2016-07-03 22:40:10 -0600

    updating sections for consistency

--- section/here/swiper-defaults.js
+++ section/here/swiper-defaults.js
@@ -7,9 +7,9 @@ var swiper = new Swiper('.swiper-container', {
   prevButton: '.swiper-button-prev',
   scrollbarHide: true,
   slidesPerView: 'auto',
-  spaceBetween: 18
-  //preloadImages: false,
-  //lazyLoading: true
+  spaceBetween: 18,
+  preloadImages: false,
+  lazyLoading: true
   //autoplay: 30000,
   //autoplayDisableOnInteraction: true
 });
