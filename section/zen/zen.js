eed538ad713b15a5fbf09b243f2af92ba3d8ca0a
Max Rubenacker <shuuki@gmail.com>
2016-07-10 19:26:17 -0600

    beautifying zen

--- section/zen/zen.js
+++ section/zen/zen.js
@@ -1,5 +1,7 @@
-var buttons = document.getElementsByClassName('pad');
-var text = document.getElementById('text');
+var buttons = document.getElementsByClassName('pad'),
+  text = document.getElementById('text'),
+  color = document.getElementById('color'),
+  colorCode = document.getElementById('colorCode');
 
 for (b in buttons) {
   if (buttons.hasOwnProperty(b)) {
@@ -9,22 +11,49 @@ for (b in buttons) {
 
       if (this.value === "clear") {
 
+        color.style.backgroundColor = "transparent";
+        colorCode.innerHTML = "";
         text.value = "";
 
       } else if (this.value === "enter") {
 
-        if (text.value.length > 0) {
+        if (text.value.length < 6) {
+
           console.log(text.value);
+          colorCode.innerHTML = "";
+          text.value = "";
+
+        } else if (text.value.length === 6) {
+
+          color.style.backgroundColor = "#"+text.value;
+          colorCode.innerHTML = "#"+text.value;
+          //text.value = "";
+
         } else {
+
           console.log("but nah");
+
         }
 
       } else {
 
         if (text.value.length < 6) {
+
           text.value = (text.value + this.value);
+
+        } else if (text.value.length === 6) {
+
+          var shifted = text.value.split("").splice(1, 6);
+          shifted.push(this.value);
+          shifted = shifted.join("");
+          text.value = shifted;
+
+          console.log(shifted);
+
         } else {
+
           console.log("nope")
+
         }
         
       }
