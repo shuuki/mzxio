5939488143953ab2ee53475daffdb88b397a8306
Max Rubenacker <shuuki@gmail.com>
2016-07-16 07:33:57 -0600

    cards update, separating styles, adding more detail to cards, now deal face down

--- section/cards/deck.js
+++ section/cards/deck.js
@@ -1,84 +1,4 @@
-var deck = [
-	{ name: "Trickster" },
-  { name: "Death" },
-	// bones
-	{ name: "Ace of Bones" },
-	{ name: "Two of Bones" },
-	{ name: "Three of Bones" },
-	{ name: "Four of Bones" },
-	{ name: "Five of Bones" },
-	{ name: "Six of Bones" },
-	{ name: "Seven of Bones" },
-	{ name: "Eight of Bones" },
-	{ name: "Nine of Bones" },
-	{ name: "Ten of Bones" },
-	{ name: "Jack of Bones" },
-	{ name: "Queen of Bones" },
-	{ name: "King of Bones" },
-	// flowers
-	{ name: "Ace of Flowers" },
-	{ name: "Two of Flowers" },
-	{ name: "Three of Flowers" },
-	{ name: "Four of Flowers" },
-	{ name: "Five of Flowers" },
-	{ name: "Six of Flowers" },
-	{ name: "Seven of Flowers" },
-	{ name: "Eight of Flowers" },
-	{ name: "Nine of Flowers" },
-	{ name: "Ten of Flowers" },
-	{ name: "Jack of Flowers" },
-	{ name: "Queen of Flowers" },
-	{ name: "King of Flowers" },
-	// fruits
-	{ name: "Ace of Fruits" },
-	{ name: "Two of Fruits" },
-	{ name: "Three of Fruits" },
-	{ name: "Four of Fruits" },
-	{ name: "Five of Fruits" },
-	{ name: "Six of Fruits" },
-	{ name: "Seven of Fruits" },
-	{ name: "Eight of Fruits" },
-	{ name: "Nine of Fruits" },
-	{ name: "Ten of Fruits" },
-	{ name: "Jack of Fruits" },
-	{ name: "Queen of Fruits" },
-	{ name: "King of Fruits" },
-	// hearts
-	{ name: "Ace of Hearts" },
-	{ name: "Two of Hearts" },
-	{ name: "Three of Hearts" },
-	{ name: "Four of Hearts" },
-	{ name: "Five of Hearts" },
-	{ name: "Six of Hearts" },
-	{ name: "Seven of Hearts" },
-	{ name: "Eight of Hearts" },
-	{ name: "Nine of Hearts" },
-	{ name: "Ten of Hearts" },
-	{ name: "Jack of Hearts" },
-	{ name: "Queen of Hearts" },
-	{ name: "King of Hearts" },
-	// arcana
-  { name: "Star" },
-  { name: "World" },
-  { name: "Sun" },
-  { name: "Moon" },
-  { name: "Priest" },
-  { name: "Temperance" },
-  { name: "Strength" },
-  { name: "Dream" },
-  { name: "Hermit" },
-  { name: "Tower" },
-  { name: "Chariot" },
-  { name: "Desire" },
-  { name: "Judgement" },
-  { name: "Devil" },
-  { name: "Hanged Man" },
-  { name: "Chance" },
-]
-
-
-
-// from mike bostock
+// shuffle from mike bostock
 // via https://www.frankmitchell.org/2015/01/fisher-yates/
 
 function shuffle(array) {
@@ -101,16 +21,119 @@ function shuffle(array) {
 
 
 
+var deck = [
+	{ name: "Trickster" },
+  { name: "Death" },
+	// bones
+	{ name: "One of Bones", peek: "1 B", pip: "1"  },
+	{ name: "Two of Bones", peek: "2 B", pip: "2"  },
+	{ name: "Three of Bones", peek: "3 B", pip: "3"  },
+	{ name: "Four of Bones", peek: "4 B", pip: "4"  },
+	{ name: "Five of Bones", peek: "5 B", pip: "5"  },
+	{ name: "Six of Bones", peek: "6 B", pip: "6"  },
+	{ name: "Seven of Bones", peek: "7 B", pip: "7"  },
+	{ name: "Eight of Bones", peek: "8 B", pip: "8"  },
+	{ name: "Nine of Bones", peek: "9 B", pip: "9"  },
+	{ name: "Ace of Bones", peek: "A B", pip: "10"  },
+	{ name: "Jack of Bones", peek: "J B", pip: "11"  },
+	{ name: "Queen of Bones", peek: "Q B", pip: "12"  },
+	{ name: "King of Bones", peek: "K B", pip: "13"  },
+	// flowers
+	{ name: "One of Flowers", peek: "", pip: "" },
+	{ name: "Two of Flowers", peek: "", pip: "" },
+	{ name: "Three of Flowers", peek: "", pip: "" },
+	{ name: "Four of Flowers", peek: "", pip: "" },
+	{ name: "Five of Flowers", peek: "", pip: "" },
+	{ name: "Six of Flowers", peek: "", pip: "" },
+	{ name: "Seven of Flowers", peek: "", pip: "" },
+	{ name: "Eight of Flowers", peek: "", pip: "" },
+	{ name: "Nine of Flowers", peek: "", pip: "" },
+	{ name: "Ace of Flowers", peek: "", pip: "" },
+	{ name: "Jack of Flowers", peek: "", pip: "" },
+	{ name: "Queen of Flowers", peek: "", pip: "" },
+	{ name: "King of Flowers", peek: "", pip: "" },
+	// fruits
+	{ name: "Ace of Fruits", peek: "", pip: "" },
+	{ name: "Two of Fruits", peek: "", pip: "" },
+	{ name: "Three of Fruits", peek: "", pip: "" },
+	{ name: "Four of Fruits", peek: "", pip: "" },
+	{ name: "Five of Fruits", peek: "", pip: "" },
+	{ name: "Six of Fruits", peek: "", pip: "" },
+	{ name: "Seven of Fruits", peek: "", pip: "" },
+	{ name: "Eight of Fruits", peek: "", pip: "" },
+	{ name: "Nine of Fruits", peek: "", pip: "" },
+	{ name: "Ten of Fruits", peek: "", pip: "" },
+	{ name: "Jack of Fruits", peek: "", pip: "" },
+	{ name: "Queen of Fruits", peek: "", pip: "" },
+	{ name: "King of Fruits", peek: "", pip: "" },
+	// hearts
+	{ name: "Ace of Hearts", peek: "", pip: "" },
+	{ name: "Two of Hearts", peek: "", pip: "" },
+	{ name: "Three of Hearts", peek: "", pip: "" },
+	{ name: "Four of Hearts", peek: "", pip: "" },
+	{ name: "Five of Hearts", peek: "", pip: "" },
+	{ name: "Six of Hearts", peek: "", pip: "" },
+	{ name: "Seven of Hearts", peek: "", pip: "" },
+	{ name: "Eight of Hearts", peek: "", pip: "" },
+	{ name: "Nine of Hearts", peek: "", pip: "" },
+	{ name: "Ten of Hearts", peek: "", pip: "" },
+	{ name: "Jack of Hearts", peek: "", pip: "" },
+	{ name: "Queen of Hearts", peek: "", pip: "" },
+	{ name: "King of Hearts", peek: "", pip: "" },
+	// arcana
+  { name: "Star", peek: "", pip: "" },
+  { name: "World", peek: "", pip: "" },
+  { name: "Sun", peek: "", pip: "" },
+  { name: "Moon", peek: "", pip: "" },
+  { name: "Priest", peek: "", pip: "" },
+  { name: "Temperance", peek: "", pip: "" },
+  { name: "Strength", peek: "", pip: "" },
+  { name: "Dream", peek: "", pip: "" },
+  { name: "Hermit", peek: "", pip: "" },
+  { name: "Tower", peek: "", pip: "" },
+  { name: "Chariot", peek: "", pip: "" },
+  { name: "Desire", peek: "", pip: "" },
+  { name: "Judgement", peek: "", pip: "" },
+  { name: "Devil", peek: "", pip: "" },
+  { name: "Hanged Man", peek: "", pip: "" },
+  { name: "Chance", peek: "", pip: "" },
+]
+
+
+// card container
+//.card
+
+// outer border
+//.border
+
+// corner symbols
+//.peek
+
+// repeated symbols
+//.pips
+
+// art
+//.art
+
+// description
+//.description
+
+
+
+
+
 function addElement(text) { 
   // create a new div element 
   // and give it some content 
   var newDiv = document.createElement("div");
-  newDiv.classList.add("card")
+  newDiv.classList.add("card", "flip") 
+  
   var newContent = document.createTextNode(text); 
   newDiv.appendChild(newContent); //add the text node to the newly created div. 
 
   // add the newly created element and its content into the DOM 
   var currentDiv = document.getElementById("table"); 
+
   currentDiv.insertAdjacentElement("beforeend", newDiv); 
 }
 
@@ -121,5 +144,19 @@ function addElement(text) {
 shuffle(deck).forEach(function(value) {
   
   addElement(value.name)
+  
 
 })
+
+
+var cards = document.getElementsByClassName("card");
+
+for (c in cards) {
+  if (cards.hasOwnProperty(c)) {
+    cards[c].addEventListener("click", function() {
+      
+      this.classList.toggle("flip")
+
+    }, false)
+  }
+}
